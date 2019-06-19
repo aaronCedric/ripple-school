a1fab26e0dc4563e5ea252cb57671c542d3c226d
aaronCedric <aaronCedric.celino@gmail.com>
2019-06-18 23:05:40 -0600

    initial commit

---
+++ footer.php
@@ -0,0 +1,71 @@
+<footer class="footer">
+  <div class="row">
+    <div class="col-md-3 my-5 text-left pl-5">
+      <a href="homepage.html">
+        <img src="img/cover-logo-red.png" alt="school-logo">
+      </a>
+      <p class="lead pt-5">Ripple School of design</p>
+      <p class="lead">8900 US-14, Crystal Lake</p>
+      <p class="lead">Illinois 60012</p>
+      <p class="lead pt-4">TEL:935-432-3219</p>
+      <p class="lead">FAX: 935-432-1254</p>
+      <p class="lead">EMAIL: contact@rippleschool.com</p>
+    </div>
+    <div class="col-md-3 my-5 text-left">
+      <nav>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link anchor-color" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link anchor-color" href="courses.html">Courses</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link anchor-color" href="gallery.html">Gallery</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link anchor-color" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>      
+    </div>
+    <div class="col-md-3 my-5 text-left pl-5">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Admission</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Admission Policies</a></li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Campus</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Alumnis</a>
+        </li>        
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Application Intl.</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Application Local</a>
+        </li>
+      </ul>      
+    </div>
+    <div class="col-md-3 my-5 text-center customIcons">
+      <a href="#" class="fontIcons">
+        <i class="fab fa-facebook-square fa-3x p-2"></i>        
+      </a>
+      <a href="#" class="fontIcons">
+        <i class="fab fa-twitter fa-3x p-2"></i>        
+      </a>
+      <a href="#" class="fontIcons">
+        <i class="fab fa-pinterest fa-3x p-2"></i>        
+      </a>
+      <a href="#" class="fontIcons">
+        <i class="fab fa-instagram fa-3x p-2"></i>        
+      </a>
+      <a href="#" class="fontIcons">
+        <i class="fab fa-google-plus-square fa-3x p-2"></i>        
+      </a>
+    </div>
+  </div>
+</footer>
---
+++ navigation.php
@@ -0,0 +1,24 @@
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+    <a href="homepage.html" class="pl-3 py-3">
+      <img src="img/cover-logo.png" alt="school-logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="courses.html">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="gallery.html">Gallery</a>
+        </li>
+        <form class="form-inline mt-2 mt-md-0">
+          <button class="btn" type="button">Contact</button>
+        </form>
+      </ul>
+    </div>
+  </nav>
